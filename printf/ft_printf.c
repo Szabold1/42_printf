abb31bef5f920f29a06014d202d492993c8ba0e5
Boldizsar Szabo <szboldisz@gmail.com>
2023-09-25 14:03:17 +0000

    Update printf and makefile

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,11 +1,38 @@
-#include "libft.h"
+#include "libft/libft.h"
+#include "libftprintf.h"
 
 int	ft_printf(const char *str, ...)
 {
+	int		count;
 	va_list args;
 
+	count = 0;
 	va_start(args, str);
+	while (*str) {
+		if (*str == '%')
+			count += handle_percent(&str, args);
+		else
+		{
+			ft_putchar_fd(*str, 1);
+			count++;
+		}
+		str++;
+	}
 	va_end(args);
-	if (handle_err())
-		return (-1);
+	// if (handle_err())
+	// 	return (-1);
+	return (count);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	char *name = "Boldi";
+	int age = 21;
+	int my_ret = ft_printf("(ft_printf) Hi, my name is %s, and I'm %d years old.\n", name, age);
+	int ret = printf("(printf)    Hi, my name is %s, and I'm %d years old.\n", name, age);
+	printf("ft_printf return: %d\n", my_ret);
+	printf("   printf return: %d\n", ret);
+	return (0);
+}
+*/
