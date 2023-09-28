2f09774586a501ae9e3ab9f8c1e322715a554451
Boldizsar Szabo <szboldisz@gmail.com>
2023-09-28 17:43:21 +0000

    Fix mistake

--- printf/files/print_ptr.c
+++ printf/files/print_ptr.c
@@ -17,6 +17,11 @@ int	print_ptr(void *ptr)
 	int	count;
 
 	count = 0;
+	if (!ptr)
+	{
+		ft_putstr_fd("(nil)", 1);
+		return (5);
+	}
 	ft_putstr_fd("0x", 1);
 	count += 2;
 	count += print_hex((unsigned long long)ptr, 'x');
